1baadf4a15ec37b43382bb83bcdc6e8c697a4db5
mateuspb <mateusperin@gmail.com>
2020-05-16 17:16:34 -0300

    Add files via upload

--- csharp-2/Source.Test/CesarCypherTest.cs
+++ csharp-2/Source.Test/CesarCypherTest.cs
@@ -19,5 +19,19 @@ namespace Codenation.Challenge
             Assert.Equal("d1e2f3g4h5i6j7k8l9m0", cypher.Crypt("a1b2c3d4e5f6g7h8i9j0"));
         }
 
+        [Fact]
+        public void Teste_Crypt()
+        {
+            var cypher = new CesarCypher();
+            Assert.Equal("whvwh1 frp hvsdfr abc", cypher.Crypt("teste1 com espaco xyz"));
+        }
+
+        [Fact]
+        public void Teste_Decrypt()
+        {
+            var cypher = new CesarCypher();
+            Assert.Equal("teste1 com espaco xyz", cypher.Decrypt("whvwh1 frp hvsdfr abc"));
+        }
+
     }
 }
