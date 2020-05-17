fe3cbc579b00245ed130e734222af3c7dbc07184
mateuspb <mateusperin@gmail.com>
2020-05-17 08:52:15 -0300

    Add files via upload

--- csharp-0/Source.Test/MathTest.cs
+++ csharp-0/Source.Test/MathTest.cs
@@ -14,6 +14,14 @@ namespace Codenation.Challenge
         }
 
         [Fact]
+        public void Fibonacci_Quantidade()
+        {
+            var math = new Math();
+            var result = math.Fibonacci();
+            Assert.True(result.Count == 14);
+        }
+
+        [Fact]
         public void Is_Fibonacci_Test()
         {
             var math = new Math();
@@ -25,6 +33,27 @@ namespace Codenation.Challenge
         {
             var math = new Math();
             Assert.False(math.IsFibonacci(4));
+        }
+
+        [Fact]
+        public void Is_NotFibonacci_Test_Negativo()
+        {
+            var math = new Math();
+            Assert.False(math.IsFibonacci(-1));
+        }
+
+        [Fact]
+        public void Is_Fibonacci_TestHi()
+        {
+            var math = new Math();
+            Assert.True(math.IsFibonacci(233));
+        }
+
+        [Fact]
+        public void Is_NotFibonacci_TestRange()
+        {
+            var math = new Math();
+            Assert.False(math.IsFibonacci(377));
         }
     }
 }
